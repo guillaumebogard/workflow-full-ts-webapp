75cfe346463eac00b11595facd81a4dcb3e98849
Guillaume Bogard <guillaume.bogardcoquard@gmail.com>
2023-02-05 09:10:31 -0800

    doc: added for API route getAllUsers()

--- back/src/controllers/users.ts
+++ back/src/controllers/users.ts
@@ -2,9 +2,17 @@ import { RequestHandler } from "express";
 
 import User from '../db/models/user';
 
+/**
+ * Get all users from the database
+ *
+ * @param req Request object
+ * @param res Response object
+ * @param next Next function
+ * @returns Response object
+ */
 export const getAllUsers: RequestHandler = async (req, res, next) => {
   const users: User[] = await User.findAll();
   
   return res.status(200)
-    .json({ message: "Users fetched successfully", data: users });
+    .json({ message: "Users successfully fetched", data: users });
 }
