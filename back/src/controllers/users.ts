79d479c37dceab731400313214f4717d10305abf
Guillaume Bogard <guillaume.bogardcoquard@gmail.com>
2023-02-06 17:28:32 -0800

    change: removed comments on routes for users

--- back/src/controllers/users.ts
+++ back/src/controllers/users.ts
@@ -2,14 +2,6 @@ import { RequestHandler } from "express";
 
 import User from '../db/models/user';
 
-/**
- * Get all users from the database
- *
- * @param req Request object
- * @param res Response object
- * @param next Next function
- * @returns Response object
- */
 export const getAllUsers: RequestHandler = async (req, res, next) => {
   const users: User[] = await User.findAll();
   
